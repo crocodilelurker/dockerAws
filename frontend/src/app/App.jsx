d10e0f95867a3871fe9aba8602deb2bf0e520bfc
crocodilelurker <swagatsahu556@gmail.com>
2026-03-30 23:10:37 +0530

    Fix frontend folder (removed submodule)

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { Editor } from "@monaco-editor/react";
+import { MonacoBinding } from "y-monaco";
+import { useRef, useMemo } from 'react';
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
+function App() {
+  const editorRef = useRef(null);
+
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const ytext = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect: true
+    });
+    const monacoBinding = new MonacoBinding(
+      ytext,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  }
+  return (
+    <>
+      <main className='h-screen w-full bg-zinc-950 text-white flex gap-4 p-4'>
+        <div className='h-full w-1/4 rounded-lg bg-zinc-800 '>
+        </div>
+        <div className='h-full w-3/4 rounded-lg bg-gray-950 overflow-hidden '>
+          <Editor
+            height="100%"
+            defaultLanguage="javascript"
+            defaultValue="// some comment"
+            theme="vs-dark"
+            onMount={handleMount}
+          />
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
