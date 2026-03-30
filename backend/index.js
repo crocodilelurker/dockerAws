aa3bb3c43704440725017558a380512bebca6f12
crocodilelurker <swagatsahu556@gmail.com>
2026-03-30 23:05:41 +0530

    base set

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require("http");
+const { Server } = require("socket.io");
+const { YSocketIO } =require("y-socket.io/dist/server");
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server( httpServer, {
+    cors:{
+        origin:"*",
+        methods:["GET", "POST"]
+    }
+})
+
+const ySocketIO =new YSocketIO(io);
+ySocketIO.initialize();
+//yjs setup complete
+
+app.get('/', (req,res) => {
+    res.status(200).json({ "message": "Health Check Route" });
+})
+const PORT = process.env.PORT || 3001;
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
