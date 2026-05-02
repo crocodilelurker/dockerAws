662883fce3126f41056489ac800ceee08ad8137e
crocodilelurker <swagatsahu556@gmail.com>
2026-05-02 11:56:35 +0530

    red

--- backend/index.js
+++ backend/index.js
@@ -23,3 +23,6 @@ const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+app.get("/new",(req,res) => {
+    return res.send("hello world")
+})
